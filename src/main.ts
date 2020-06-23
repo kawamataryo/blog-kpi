e868c8a63861cef287ea7dcf91537616c57a930a
kawamataryou <ba068082@gmail.com>
2020-06-23 20:50:45 +0900

    [from now] 2020/06/23 20:50:45
    diff --git a/src/main.ts b/src/main.ts
    index 84691db..be04759 100644
    --- a/src/main.ts
    +++ b/src/main.ts
    @@ -127,21 +127,27 @@ class HatenaClient {
       constructor(private blogUrl: string) {}
    
       fetchKpi(): HatenaKpi {
    -    // TODO: 他に良いAPI見つかれば差し替える
    +    const bookmarkCount = this.fetchBookmarkCount();
    +
    +    return {
    +      bookmarkCount,
    +    };
    +  }
    +
    +  private fetchBookmarkCount() {
         const redirectUrl = this.getRedirectUrl(
           `${this.BASE_URL}/bc/${this.blogUrl}`
         );
    -    // `https://b.st-hatena.com/images/counter/default/00/00/0000653.gif` の形式でブクマ数が書かれたgif画像のURLからブクマ数を取得する
    -    const countFilenname = redirectUrl.match(
    +    // `https://b.st-hatena.com/images/counter/default/00/00/0000653.gif` の形式でブクマ数が書かれたgif画像のURLを取得できるので、
    +    // そこからブクマ数を抽出する
    +    const bookmarkCount = redirectUrl.match(
           /https:\/\/b.st-hatena\.com\/images\/counter\/default\/\d+\/\d+\/(\d+).gif/
         )![1];
    
    -    return {
    -      bookmarkCount: Number(countFilenname),
    -    };
    +    return Number(bookmarkCount);
       }
    
    -  getRedirectUrl(url: string): string {
    +  private getRedirectUrl(url: string): string {
         const response = UrlFetchApp.fetch(url, {
           followRedirects: false,
           muteHttpExceptions: false,

--- src/main.ts
+++ src/main.ts
@@ -127,21 +127,27 @@ class HatenaClient {
   constructor(private blogUrl: string) {}
 
   fetchKpi(): HatenaKpi {
-    // TODO: 他に良いAPI見つかれば差し替える
-    const redirectUrl = this.getRedirectUrl(
-      `${this.BASE_URL}/bc/${this.blogUrl}`
-    );
-    // `https://b.st-hatena.com/images/counter/default/00/00/0000653.gif` の形式でブクマ数が書かれたgif画像のURLからブクマ数を取得する
-    const countFilenname = redirectUrl.match(
-      /https:\/\/b.st-hatena\.com\/images\/counter\/default\/\d+\/\d+\/(\d+).gif/
-    )![1];
+    const bookmarkCount = this.fetchBookmarkCount();
 
     return {
-      bookmarkCount: Number(countFilenname),
+      bookmarkCount,
     };
   }
 
-  getRedirectUrl(url: string): string {
+  private fetchBookmarkCount() {
+    const redirectUrl = this.getRedirectUrl(
+      `${this.BASE_URL}/bc/${this.blogUrl}`
+    );
+    // `https://b.st-hatena.com/images/counter/default/00/00/0000653.gif` の形式でブクマ数が書かれたgif画像のURLを取得できるので、
+    // そこからブクマ数を抽出する
+    const bookmarkCount = redirectUrl.match(
+      /https:\/\/b.st-hatena\.com\/images\/counter\/default\/\d+\/\d+\/(\d+).gif/
+    )![1];
+
+    return Number(bookmarkCount);
+  }
+
+  private getRedirectUrl(url: string): string {
     const response = UrlFetchApp.fetch(url, {
       followRedirects: false,
       muteHttpExceptions: false,
